85e8d0be8a72c8b11f7b861c891c26e9b73bbe4e
Nandinikuncham <nandinikuncham8@gmail.com>
2026-05-02 00:50:05 +0530

    Fix deployed backend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://future-fs-02-542u.onrender.com",
+  baseURL: "https://future-fs-02-542u.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
